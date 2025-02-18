93f98344e3e819be8b560cb2f10844f9f085c0a8
Azzy <146315215+Azzy-Gm@users.noreply.github.com>
2025-02-18 12:53:07 +0700

    Update game.js

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var Snake = (function() {
   var intervalID;
 
   var tileCount = 10;
-  var gridSize = 100 / tileCount;
+  var gridSize = 400 / tileCount;
 
   const INITIAL_PLAYER = { x: Math.floor(tileCount / 2), y: Math.floor(tileCount / 2) };
 
